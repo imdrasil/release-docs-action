d5a19a97456e1a3999a7e0a1277cbf0fab177cfb
Roman Kalnytskyi <moranibaca@gmail.com>
2021-07-16 12:07:41 +0300

    Fix project pulling

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,13 +1,12 @@
 #!/bin/ruby
 
 doc_key = ARGV[1]
-project_path = ARGV[0]
-project_name = project_path.split('/').last
+project_name = ARGV[0]
 
 File.open('id_rsa', 'w') { |f| f << doc_key }
 puts `chmod 600 id_rsa`
 puts `ssh-add id_rsa`
-puts `git clone git@github.com:#{project_path}.git`
+puts `git clone git@github.com:/imdrasil/imdrasil.github.io.git`
 Dir.chdir(project_name) do
   puts `bundle install`
   puts `./scripts/deploy.sh #{project_name}`

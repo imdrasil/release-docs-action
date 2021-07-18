2e5c6a92b48b0036a0032f42a78e9e9bb66275bc
Roman Kalnytskyi <moranibaca@gmail.com>
2021-07-19 01:36:25 +0300

    add git author

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ chmod 600 /root/.ssh/id_rsa
 git clone git@github.com:/imdrasil/imdrasil.github.io.git
 cd imdrasil.github.io
 bundle install
+git config user.email "moranibaca@gmail.com"
+git config user.name "Integration"
 bash ./scripts/deploy.sh "$1" "$3" "$4"
